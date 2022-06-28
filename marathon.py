2b1fe217a5a51cfdc1478c56918cab5c38ca518f
DariusG101 <107132490+DariusG101@users.noreply.github.com>
2022-06-28 10:20:39 -0500

    finished project 1

--- marathon.py
+++ marathon.py
@@ -24,7 +24,17 @@ def fitness():
 
     # This ignores our code so that we do not get an error Remove 'pass' when you want to start testing code.
     
-    pass
+    print("Tokyo Marathon Qualifier")
+  name = input("what is your name: ")
+  mile_every_minute = float(input("How many miles can you run in 10 minutes?"))
+  money_saved = input("How much U.S.$ do you have saved for the marathon? ")
+  money_saved = float(money_saved)
+  yen_saved = money_saved * JAPENESE_YEN
+  km_in_one_minute_pace = mile_every_minute/10 * METERS_IN_MILE / 1000
+  space_index = name.find(" ")
+  name = name[space_index +1:]
+  print(f"Dear {name}, you have a pace of {km_in_one_minute_pace:,.2f} km/min.\nAdditionally, you only have       {yen_saved:,.2f}¥ to spend ")
+
     
 
 # This invokes the main function.  It is always included in our
